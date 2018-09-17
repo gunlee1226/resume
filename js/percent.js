4b5fcff3ab117bcb808ced4825361aa65fdddb2a
chlee125 <chlee125@gmail.com>
2018-09-18 02:11:46 +0900

    add meta data add folder css, js

---
+++ js/percent.js
@@ -0,0 +1,19 @@
+$("document").ready(function() {
+  // Front-end
+  $("#HTML").css("width", "100%");
+  $("#CSS").css("width", "90%");
+  $("#JQUERY").css("width", "90%");
+  $("#JAVASCRIPT").css("width", "80%");
+  $("#JSP").css("width", "70%");
+  $("#CORDOVA").css("width", "50%");
+
+  // Back-end
+  $("#CPP").css("width", "90%");
+  $("#JAVA").css("width", "85%");
+  $("#PYTHON").css("width", "65%");
+  $("#DJANGO").css("width", "10%");
+  $("#SPRING").css("width", "80%");
+  // Data-base
+  $("#ORACLE").css("width", "65%");
+  $("#MYSQL").css("width", "25%");
+});
