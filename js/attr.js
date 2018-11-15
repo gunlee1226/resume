6019a38f2948620179c9cdc377dd27d6ed8b6922
chlee125 <chlee125@gmail.com>
2018-11-15 14:15:35 +0900

    불필요한 파일 삭제, progress bar 색상 변경

---
+++ js/attr.js
@@ -0,0 +1,4 @@
+$("document").ready(function() {
+  // Progress bar 색 변경
+  $(".progress-bar").addClass("bg-success");
+});
--- js/percent.js
+++ js/percent.js
@@ -1,18 +1,21 @@
 $("document").ready(function() {
   // Front-end
-  $("#HTML").css("width", "100%");
-  $("#CSS").css("width", "90%");
-  $("#JQUERY").css("width", "90%");
+  $("#HTML").css("width", "80%");
+  $("#CSS").css("width", "80%");
+  $("#JQUERY").css("width", "80%");
   $("#JAVASCRIPT").css("width", "80%");
   $("#JSP").css("width", "70%");
   $("#CORDOVA").css("width", "50%");
+  $("#NODE").css("width", "30%");
+  $("#VUE").css("width", "30%");
 
   // Back-end
-  $("#CPP").css("width", "90%");
+  $("#CPP").css("width", "85%");
   $("#JAVA").css("width", "85%");
+  $("#SPRING").css("width", "70%");
   $("#PYTHON").css("width", "65%");
   $("#DJANGO").css("width", "10%");
-  $("#SPRING").css("width", "80%");
+
   // Data-base
   $("#ORACLE").css("width", "65%");
   $("#MYSQL").css("width", "25%");
