897c67b106e536a61630f9dbccfc29f8181a84ae
chlee125 <chlee125@gmail.com>
2018-12-03 22:58:54 +0900

    Kotlin 공부중

--- js/percent.js
+++ js/percent.js
@@ -2,21 +2,22 @@ $("document").ready(function() {
   $(".progress-bar").css("width", "0%");
   // Front-end
   $("#HTML").css("width", "80%");
-  $("#CSS").css("width", "80%");
-  $("#JQUERY").css("width", "80%");
-  $("#JAVASCRIPT").css("width", "80%");
+  $("#CSS").css("width", "60%");
+  $("#JQUERY").css("width", "75%");
+  $("#JAVASCRIPT").css("width", "75%");
   $("#JSP").css("width", "70%");
   $("#CORDOVA").css("width", "50%");
-  $("#NODE").css("width", "30%");
   $("#VUE").css("width", "30%");
   // Back-end
-  $("#CPP").css("width", "85%");
+  $("#CPP").css("width", "75%");
   $("#JAVA").css("width", "85%");
   $("#SPRING").css("width", "70%");
   $("#PYTHON").css("width", "65%");
+  $("#NODE").css("width", "30%");
   $("#DJANGO").css("width", "10%");
+  $("#KOTLIN").css("width", "25%");
 
   // Data-base
-  $("#ORACLE").css("width", "65%");
-  $("#MYSQL").css("width", "25%");
+  $("#ORACLE").css("width", "55%");
+  $("#MYSQL").css("width", "20%");
 });
