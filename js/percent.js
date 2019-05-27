a4e7b93bfc6217956d1a7f0bc5cd5e47336e6e68
chlee125 <chlee125@gmail.com>
2019-05-27 10:13:14 +0900

    2019.05.27

--- js/percent.js
+++ js/percent.js
@@ -8,16 +8,18 @@ $("document").ready(function() {
   $("#JSP").css("width", "70%");
   $("#CORDOVA").css("width", "50%");
   $("#VUE").css("width", "30%");
+  $("#REACT").css("width", "20%");
   // Back-end
   $("#CPP").css("width", "75%");
   $("#JAVA").css("width", "85%");
   $("#SPRING").css("width", "70%");
   $("#PYTHON").css("width", "65%");
-  $("#NODE").css("width", "30%");
+  $("#NODE").css("width", "55%");
   $("#DJANGO").css("width", "10%");
   $("#KOTLIN").css("width", "25%");
 
   // Data-base
   $("#ORACLE").css("width", "55%");
   $("#MYSQL").css("width", "20%");
+  $("#MSSQL").css("width", "20%");
 });
