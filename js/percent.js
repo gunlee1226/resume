a2b4c9c6d5f94dee9ee2f75c0e649e825747ca0d
chlee125 <chlee125@gmail.com>
2019-07-22 22:52:46 +0900

    이력서 업데이트

--- js/percent.js
+++ js/percent.js
@@ -1,23 +1,44 @@
-$("document").ready(function() {
-  $(".progress-bar").css("width", "0%");
+$(".progress-bar").css("width", "0%");
+$("document").ready(function () {
   // Front-end
-  $("#HTML").css("width", "80%");
-  $("#CSS").css("width", "60%");
+  $("#HTML").css("width", "85%");
+  $("#JAVASCRIPT").css("width", "85%");
   $("#JQUERY").css("width", "75%");
-  $("#JAVASCRIPT").css("width", "75%");
   $("#JSP").css("width", "70%");
+  $("#CSS").css("width", "60%");
+  $("#VUE").css("width", "50%");
   $("#CORDOVA").css("width", "50%");
-  $("#VUE").css("width", "30%");
+
   // Back-end
+  $("#NODE").css("width", "85%");
+  $("#JAVA").css("width", "80%");
   $("#CPP").css("width", "75%");
-  $("#JAVA").css("width", "85%");
   $("#SPRING").css("width", "70%");
   $("#PYTHON").css("width", "65%");
-  $("#NODE").css("width", "30%");
-  $("#DJANGO").css("width", "10%");
+  $("#DJANGO").css("width", "30%");
   $("#KOTLIN").css("width", "25%");
 
   // Data-base
+  $("#MSSQL").css("width", "65%");
   $("#ORACLE").css("width", "55%");
-  $("#MYSQL").css("width", "20%");
+  $("#MYSQL").css("width", "25%");
+
+
+  $("#CPP").text(document.getElementById('CPP').style.width)
+  $("#JAVA").text(document.getElementById('JAVA').style.width)
+  $("#SPRING").text(document.getElementById('SPRING').style.width)
+  $("#PYTHON").text(document.getElementById('PYTHON').style.width)
+  $("#NODE").text(document.getElementById('NODE').style.width)
+  $("#DJANGO").text(document.getElementById('DJANGO').style.width)
+  $("#KOTLIN").text(document.getElementById('KOTLIN').style.width)
+  $("#HTML").text(document.getElementById('HTML').style.width)
+  $("#CSS").text(document.getElementById('CSS').style.width)
+  $("#JQUERY").text(document.getElementById('JQUERY').style.width)
+  $("#JAVASCRIPT").text(document.getElementById('JAVASCRIPT').style.width)
+  $("#JSP").text(document.getElementById('JSP').style.width)
+  $("#CORDOVA").text(document.getElementById('CORDOVA').style.width)
+  $("#VUE").text(document.getElementById('VUE').style.width);
+  $("#ORACLE").text(document.getElementById('ORACLE').style.width)
+  $("#MSSQL").text(document.getElementById('MSSQL').style.width)
+  $("#MYSQL").text(document.getElementById('MYSQL').style.width)
 });
