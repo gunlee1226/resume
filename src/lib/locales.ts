396e796c007925a71665c272348acf8429c409dd
Lee, Chungho <chlee125@gmail.com>
2024-01-05 13:19:39 +0900

    fix: locales base url

--- src/lib/locales.ts
+++ src/lib/locales.ts
@@ -15,7 +15,7 @@ i18n
       escapeValue: false,
     },
     backend: {
-      loadPath: '/locales/{{lng}}.yml',
+      loadPath: '/resume/locales/{{lng}}.yml',
       parse: function (data: string) {
         return yaml.load(data)
       },
