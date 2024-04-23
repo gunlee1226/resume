66940fae1025cd8473a602c1ffd488a5644c00b0
이건호 <gunlee1226>
2024-04-23 17:20:59 +0900

    UPDATE - 그리드 조정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,22 +9,28 @@ export default function Home() {
   return (
     <div>
       <div className="md:flex">
+  
         <div className="md:w-1/2 xl:w-1/2">
           <AboutMe />
-          <Information documents={t("education", { returnObjects: true })} />
         </div>
+        
         <div className="md:w-1/2 xl:flex xl:w-full">
+        
+          <div className="md:pl-2 xl:w-full">
+            <Information documents={t("education", { returnObjects: true })} />
+          </div>
+        
           <div className="md:pl-2 xl:w-full">
             <Information
               documents={t("experiences", { returnObjects: true })}
             />
-          </div>
-          <div className="md:pl-2 xl:w-full">
             <Information
               documents={t("certificates", { returnObjects: true })}
             />
           </div>
+        
         </div>
+      
       </div>
       <Skills />
       <Information documents={t("portfolio", { returnObjects: true })} />
